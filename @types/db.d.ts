c358541315f4917689d16ed5764a3782df6a6bd8
parkcool <yickle1345@naver.com>
2024-12-21 13:04:10 +0000

    Chore: Write db schema on `db.d.ts`

--- @types/db.d.ts
+++ @types/db.d.ts
@@ -1,9 +1,38 @@
 declare module "db" {
   interface UserRow {
     id: number;
-    email: string;
-    name: string;
+    username: string;
+    createdAt: string;
     password: string;
     salt: string;
+  }
+
+  interface Topic {
+    id: number;
+    name: string;
+  }
+
+  interface moment {
+    id: number;
+    userId: number;
+    createdAt: string;
+    expiresAt?: string;
+    text: string;
+  }
+
+  interface moment_photo {
+    momentId: number;
+    path: number;
+  }
+
+  interface moment_topic {
+    momentId: number;
+    topicId: number;
+  }
+
+  interface moment_reaction {
+    momentId: number;
+    userId: number;
+    emoji: string;
   }
 }
---
+++ momenty.sql
@@ -0,0 +1,46 @@
+CREATE TABLE `user` (
+  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) UNIQUE NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT (NOW()),
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL
+);
+
+CREATE TABLE `topic` (
+  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL
+);
+
+CREATE TABLE `moment` (
+  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `userId` integer NOT NULL,
+  `createdAt` timestamp NOT NULL,
+  `expiresAt` timestamp,
+  `text` varchar(255) NOT NULL
+);
+
+CREATE TABLE `moment_photo` (
+  `momentId` integer NOT NULL,
+  `path` varchar(255) NOT NULL
+);
+
+CREATE TABLE `moment_topic` (
+  `momentId` integer NOT NULL,
+  `topicId` integer NOT NULL
+);
+
+CREATE TABLE `moment_reaction` (
+  `momentId` integer NOT NULL,
+  `userId` integer NOT NULL,
+  `emoji` varchar(255) NOT NULL
+);
+
+ALTER TABLE `moment` ADD FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `moment_photo` ADD FOREIGN KEY (`momentId`) REFERENCES `moment` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `moment_topic` ADD FOREIGN KEY (`momentId`) REFERENCES `moment` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `moment_reaction` ADD FOREIGN KEY (`momentId`) REFERENCES `moment` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `moment_topic` ADD FOREIGN KEY (`topicId`) REFERENCES `topic` (`id`) ON DELETE CASCADE;
